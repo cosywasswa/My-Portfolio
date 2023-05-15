b1a5bf46fed821c3aa7f8b026334ed4b2bca364f
Cosmas Wasswa <cosywas@gmail.com>
2023-05-15 17:57:07 +0300

    Corrected .js linters

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-mobile");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-mobile');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-document.querySelectorAll(".link").forEach(n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-}))
+document.querySelectorAll('.link').forEach((n) => n
+  .addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
