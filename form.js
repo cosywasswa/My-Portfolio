0a0876bd6acdfe2899ce519db041414ae9c0e7fd
Cosmas Wasswa <cosywas@gmail.com>
2023-05-19 12:50:21 +0300

    Corrected linters in javascript after implementing data storage

--- form.js
+++ form.js
@@ -1,46 +1,44 @@
 const form = document.querySelector('form');
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const emailInput = document.querySelector('#email');
-    const email = emailInput.value;
+  const emailInput = document.querySelector('#email');
+  const email = emailInput.value;
 
-    if (email === email.toLowerCase()) {
-        form.submit();
-    } else {
-        let Message = form.querySelector('.massage-error');
-        if (Message) {
-            form.removeChild(Message);
-        }
-        Message = document.createElement('p');
-        Message.classList.add('massage-error');
-        Message.textContent = 'Please, your email address must be in lowercase.';
-        form.appendChild(Message);
+  if (email === email.toLowerCase()) {
+    form.submit();
+  } else {
+    let Message = form.querySelector('.massage-error');
+    if (Message) {
+      form.removeChild(Message);
     }
+    Message = document.createElement('p');
+    Message.classList.add('massage-error');
+    Message.textContent = 'Please, your email address must be in lowercase.';
+    form.appendChild(Message);
+  }
 }
 
 form.addEventListener('submit', handleSubmit);
-
 
 const yourName = document.getElementById('full-name');
 const yourEmail = document.getElementById('email');
 const yourMassage = document.getElementById('textmsg');
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formDataStorage = {
-        yourName: yourName.value,
-        yourEmail: yourEmail.value,
-        yourMassage: yourMassage.value,
-    };;
+  const formDataStorage = {
+    yourName: yourName.value,
+    yourEmail: yourEmail.value,
+    yourMassage: yourMassage.value,
+  };
 
-    localStorage.setItem('formData', JSON.stringify(formDataStorage));
+  localStorage.setItem('formData', JSON.stringify(formDataStorage));
 });
 
 window.addEventListener('load', () => {
-    const contactDataRecover = JSON.parse(localStorage.getItem('formData'));
-    yourName.value = contactDataRecover.yourName;
-    yourEmail.value = contactDataRecover.yourEmail;
-    yourMassage.value = contactDataRecover.yourMassage;
-
+  const contactDataRecover = JSON.parse(localStorage.getItem('formData'));
+  yourName.value = contactDataRecover.yourName;
+  yourEmail.value = contactDataRecover.yourEmail;
+  yourMassage.value = contactDataRecover.yourMassage;
 });
